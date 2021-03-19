79264f94da883abbb5687effa5de12c75d0f440a
David Valles <davidtjvalles@gmail.com>
2021-03-19 15:08:07 -0700

    Fix format errors

--- src/py_demo/increment.py
+++ src/py_demo/increment.py
@@ -1,8 +1,9 @@
 def increment(num):
     return num + 1
 
+
 def increment_by_step(num, step=1):
-    if (step == 1):
+    if step == 1:
         return increment(num)
 
     return num + step
--- tests/test_increment.py
+++ tests/test_increment.py
@@ -8,22 +8,24 @@ from src.py_demo.increment import increment, increment_by_step
 def test_should_increment_by_1():
     assert increment(3) == 4
 
+
 def test_should_increment_by_default_step():
     assert increment_by_step(3) == 4
 
 
 # manually define all cases. probably gonna miss something!
 @pytest.mark.parametrize(
-    'num, result',
+    "num, result",
     [
         (-2, -1),
         (0, 1),
         (3, 4),
         (101234, 101235),
-    ]
+    ],
 )
 def test_increment(num, result):
     assert increment(num) == result
+
 
 # hypothesis generates way more edge cases!
 @given(st.integers())
